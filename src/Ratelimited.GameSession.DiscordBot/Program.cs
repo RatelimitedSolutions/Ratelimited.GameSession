14a47b68bd2c88a3e53ae59ec36de532d7301a4d
CreedsCode <hoffmann.dercio@hotmail.com>
2020-04-06 01:56:50 +0200

    Add Discord Bot

---
+++ src/Ratelimited.GameSession.DiscordBot/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Ratelimited.GameSession.DiscordBot.Services;
+
+namespace Ratelimited.GameSession.DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+
+        public async Task MainAsync()
+        {
+            using (var services = ConfigureServices())
+            {
+                var client = services.GetRequiredService<DiscordSocketClient>();
+
+                client.Log += LogAsync;
+                services.GetRequiredService<CommandService>().Log += LogAsync;
+
+                await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("TOKEN"));
+                await client.StartAsync();
+
+                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+
+                await Task.Delay(-1);
+            }
+        }
+
+
+        private Task LogAsync(LogMessage log)
+        {
+            var date = new DateTime();
+            Console.WriteLine($"[{date.ToLocalTime()}] {log.ToString()}");
+
+            return Task.CompletedTask;
+        }
+
+        private ServiceProvider ConfigureServices()
+        {
+            return new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>()
+                .AddSingleton<CommandService>()
+                .AddSingleton<CommandHandlingService>()
+                .AddSingleton<HttpClient>()
+                .AddSingleton<PictureService>()
+                .BuildServiceProvider();
+        }
+
+    }
+}
---
+++ src/Ratelimited.GameSession.DiscordBot/Services/CommandHandlingService.cs
@@ -0,0 +1,61 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ratelimited.GameSession.DiscordBot.Services
+{
+    public class CommandHandlingService
+    {
+        private CommandService _commands;
+        private DiscordSocketClient _discord;
+        private IServiceProvider _services;
+
+        public CommandHandlingService(IServiceProvider services)
+        {
+            _commands = services.GetRequiredService<CommandService>();
+            _discord = services.GetRequiredService<DiscordSocketClient>();
+            _services = services;
+
+            _commands.CommandExecuted += CommandExecutedAsync;
+
+            _discord.MessageReceived += MessageReceivedAsync;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage socketMessage)
+        {
+            if(!(socketMessage is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
+
+            int argPossition = 0;
+            if (!message.HasCharPrefix('!', ref argPossition)) return;
+
+            SocketCommandContext context = new SocketCommandContext(_discord, message);
+
+            await _commands.ExecuteAsync(context, argPossition, _services);
+        }
+
+        private async Task CommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            // Not found
+            if (!command.IsSpecified)
+                return;
+
+            if (result.IsSuccess)
+                return;
+
+            await context.Channel.SendMessageAsync($"Error: {result}");
+        }
+    }
+}
